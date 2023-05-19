82cda46072151bf091e4df8d8ac0374ddba9520f
danakim530 <danakim530@gmail.com>
2023-05-19 23:54:16 +0900

    06_class.tsx

---
+++ 06_Class.tsx
@@ -0,0 +1,28 @@
+// ES6의 클래스가 익숙하다는 가정하에 수업
+
+// 예제1
+class Car {
+  color: String; // 미리 선언해야 함
+  constructor(color: string) {
+    this.color = color;
+  }
+  start() {
+    console.log('start');
+  }
+}
+const bmw = new Car('red');
+
+// 예제2
+class Car2 {
+  // color: String; 여기 주석처리
+  constructor(public color: string) {
+    // public or readonly 쓰기
+    this.color = color;
+  }
+  start() {
+    console.log('start');
+  }
+}
+const bmw2 = new Car('red');
+
+// 접근 제한자(Access modifier) - public, private, protected
