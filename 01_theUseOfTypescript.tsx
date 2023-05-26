3ad0872b8e2ca73a1529528bb0840ef7a0df1a31
danakim530 <danakim530@gmail.com>
2023-05-27 00:54:54 +0900

    test

--- 01_theUseOfTypescript.tsx
+++ 01_theUseOfTypescript.tsx
@@ -1,8 +1,8 @@
-// Youtube 코딩앙마 TypeScript 강좌 
+// Youtube 코딩앙마 TypeScript 강좌
 // 자바스크립트 : 동적언어 (변수 선언시 타입 지정X)
 // 타입스크립트 : 정적언어 (변수 선언시 타입 지정O) -> 해당 타입이 아닌 데이터를 넘기려고 하면 타입스트립트 컴파일러가 에러를 뱉음
 // Browser can't execute TS so TS is compiled to JS
-// TS(.ts or .tsx) = Compiler
+// TS(확장자 .ts or .tsx) = Compiler
 // TS 쓰려면, install node.js(npm 사용 위함) + sudo npm install -g typescript 설치
 
 // 예제1)___________________________________
