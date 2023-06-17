9089ef3d5dec138ca724467e355c07f3a7972d64
danakim530 <danakim530@gmail.com>
2023-06-17 14:53:35 +0900

    add : generic vs any

--- 08_generic.tsx
+++ 08_generic.tsx
@@ -1,72 +1,76 @@
-// Generic 
+// Generic
+// Generic과 Any의 차이?
+// Any는 에러 못잡음. Generic은 들어오는 파라미터에 따라 타입이 픽스됨. 이상한 값 들어오면 에러 남.
 
 // 예제1) Function에서 제네릭 쓰는법
 // 이렇게 파라미터 종류가 다양한 상황에서 사용_____________________________________________
-function getSize(arr: number[] | string[] | boolean[] | object[]): number{
-    return arr.length;
+function getSize(arr: number[] | string[] | boolean[] | object[]): number {
+  return arr.length;
 }
-const arr1 = [1,2,3];
+const arr1 = [1, 2, 3];
 getSize(arr1); //3
 
-const arr2= ['a','b','c']
+const arr2 = ['a', 'b', 'c'];
 getSize(arr2); //3
 
-const arr3= [false, true, true]
+const arr3 = [false, true, true];
 getSize(arr3); //3
 
-const arr4 = [{}, {}, {name: 'Tim'}];
+const arr4 = [{}, {}, { name: 'Tim' }];
 getSize(arr4); //3
 
 // Generic 사용법 (T 이름은 바꿔도 됨)__________________________________________
-function getSize2<T>(arr: T[]): number{ //배열정도만 설정
-    return arr.length;
+function getSize2<T>(arr: T[]): number {
+  //배열정도만 설정
+  return arr.length;
 }
-const arr5 = [1,2,3];
+const arr5 = [1, 2, 3];
 getSize2<number | string>(arr1); //3 여기 기입안해도 사실 무슨 타입인지 알고있긴함
 
-const arr6= ['a','b','c']
+const arr6 = ['a', 'b', 'c'];
 getSize2<string>(arr2); //3
 
 //예제2) Interface에서 제네릭 쓰는법__________________________________________
-interface Mobile3<T>{
-    name:string;
-    price: number;
-    option:T;
+interface Mobile3<T> {
+  name: string;
+  price: number;
+  option: T;
 }
-const m1: Mobile3<{color:string; coupon:boolean}>={
-    name:'s21',
-    price: 1000,
-    option:{
-        color:'red',
-        coupon:false
-    },
-}
-const m2: Mobile3<string>={
-    name:'s21',
-    price: 1000,
-    option:'good',
-}
+const m1: Mobile3<{ color: string; coupon: boolean }> = {
+  name: 's21',
+  price: 1000,
+  option: {
+    color: 'red',
+    coupon: false,
+  },
+};
+const m2: Mobile3<string> = {
+  name: 's21',
+  price: 1000,
+  option: 'good',
+};
 
 //예제3) 객체에서 제네릭 쓰는법_________________________________________________
-interface User{
-    name: string;
-    age: number;
+interface User {
+  name: string;
+  age: number;
 }
-interface Car{
-    name: string;
-    color: string;
+interface Car {
+  name: string;
+  color: string;
 }
-interface Book{
-    price: number;
+interface Book {
+  price: number;
 }
-const user:User = {name: 'a', age:10};
-const car:Car = {name: 'bmw', color: 'red'}
-const book : Book = {price:3000};
+const user: User = { name: 'a', age: 10 };
+const car: Car = { name: 'bmw', color: 'red' };
+const book: Book = { price: 3000 };
 
-// book은 name이 없어서 사용안됨 
+// book은 name이 없어서 사용안됨
 // 에러 발생 : function showName<T>(data:T):string{
-function showName<T extends {name:string}>(data:T):string{ // T타입이 올건데 모두가 name:string 있도록 확장 
-    return data.name; //객체의 name프로퍼티 리턴
+function showName<T extends { name: string }>(data: T): string {
+  // T타입이 올건데 모두가 name:string 있도록 확장
+  return data.name; //객체의 name프로퍼티 리턴
 }
 showName(user);
 showName(car);
